3e919336e0c2d7770e6f2d99a091cac397e7a120
ritik687 <200495974@student.georgianc.on.ca>
2023-07-28 07:51:12 -0400

    adding some css to the navbar

--- basicCode/models/user.js
+++ basicCode/models/user.js
@@ -1,10 +1,10 @@
-// 1) Create a model the same way we created the other models
+//  Create a model the same way we created the other models
 const mongoose = require('mongoose');
 
-// 2) Inject authentication related functionality by injecting the passport module
+// Inject authentication related functionality by injecting the passport module
 const plm = require('passport-local-mongoose');
 
-// PART 1 Google Auth: Add SecretKey and twoFAMethod to User Model
+
 const schemaDefinition = {
     username: String,
     password: String,
