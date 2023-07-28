acd746caf038caf06f3dad61bddb0bb396d12aec
ritik687 <200495974@student.georgianc.on.ca>
2023-07-28 07:08:36 -0400

    adding the global configuration

---
+++ basicCode/config/globals.js
@@ -0,0 +1,7 @@
+//JSON Object that contains my gloabal configuration values
+const configuration = {
+  'db': 'mongodb+srv://admin:password200495974@cluster0.sky7vf2.mongodb.net/Two-Factor-Auth-Sample'
+
+}
+
+module.exports = configuration;
