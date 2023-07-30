055f7d22adc4ad2013b899e6c31eb19dd8c7b768
ritik687 <200495974@student.georgianc.on.ca>
2023-07-30 16:00:00 -0400

    making basic code and demo code working properly  before 2 factor auth

--- demoFinalCode/routes/index.js
+++ demoFinalCode/routes/index.js
@@ -8,7 +8,8 @@ var passport = require('passport');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { 
-    title: 'Two-Factor Authentication' 
+    title: 'Two-Factor Authentication',
+    user: req.user
   });
 });
 /* Routes for the login */
@@ -20,13 +21,13 @@ router.get('/login', (req, res, next) => {
   res.render('login', {
     title: 'Login',
     messages: messages,
-    user: req.user
+    // user: req.user
   });
 });
 
 // post handler
 router.post('/login', passport.authenticate('local', {
-  successRedirect: '/loginSuccess',
+  successRedirect: '/loggedIn',
   failureRedirect: '/login',
   failureMessage: 'Invalid Credentials'
 }));
@@ -36,7 +37,7 @@ router.post('/login', passport.authenticate('local', {
 router.get('/register', (req, res, next) => {
   res.render('register', {
     title: 'Register',
-    user: req.user
+    //user: req.user
   });
 });
 
@@ -47,26 +48,66 @@ router.post('/register', (req, res, next) => {
       username: req.body.username
     }),
     req.body.password,
+
     (err, newUser) => {
       if (err) {
         console.log(err);
         return res.redirect('/register');
-      } else {
-        req.login(newUser, (err) => {
-          res.redirect('/loginSuccess');
-        });
+      } 
+      else {
+        // req.login(newUser, (err) => {
+        //   res.redirect('/loggedIn');
+        // });
+        res.redirect('/registered');
       }
     }
   );
 });
 
 
+
+// get handler for the registered user.
 router.get('/registered', (req,res,next)=>{
   res.render('registered',{
     title: "Successfully Registered"
   });
 
 });
+
+
+// get handler for the logout user
+router.get('/logout', (req, res, next) => {
+  
+
+  // log user out
+  req.logout(function(err){
+    if(err)
+    {
+      console.log(err);
+      return next(err);
+    }
+  });
+  // send user back to login page
+  res.redirect('/login');
+});
+
+
+//get handler to check if the current user is logged in or not and also check if the current use has initialized 2FA and if they are verified
+router.get('/loggedIn', 
+(req, res, next)=>{
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect('/login');
+}, 
+(req, res, next)=>{
+  res.render('loggedIn', {
+    title: 'Login Successful',
+    user: req.user
+  });
+}
+
+)
 
 
 module.exports  =  router;
