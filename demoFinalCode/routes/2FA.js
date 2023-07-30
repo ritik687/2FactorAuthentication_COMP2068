407cf5abf62cc5753b42d48917948419956881e9
ritik687 <200495974@student.georgianc.on.ca>
2023-07-30 18:42:26 -0400

    adding the post handler when the code is entered

--- demoFinalCode/routes/2FA.js
+++ demoFinalCode/routes/2FA.js
@@ -2,11 +2,12 @@ var express = require('express')
 var router = express.Router();
 
 // importing the User model for authentication
-var User = require('../models/user');
+const User = require('../models/user');
 
 
 // importing  npm module -- npm i  speakeasy
 var speakeasy = require("speakeasy");
+const { token } = require('morgan');
 
 
 
@@ -19,13 +20,13 @@ function isUserLoggedIn(req,res,next){
 
 router.get('/', isUserLoggedIn, (req,res,next)=>{
   
-        var encodedKey = speakeasy.generateSecret({
+        var secretKeyObject = speakeasy.generateSecret({
           name: req.user.username
       })
-      console.log(encodedKey);
-
-      var otpURL = encodedKey.otpauth_url;
-
+      
+      var otpURL = secretKeyObject.otpauth_url;
+      
+      console.log(secretKeyObject);
       console.log(otpURL);
      
 
@@ -37,13 +38,60 @@ router.get('/', isUserLoggedIn, (req,res,next)=>{
       req.session.messages = [];
 
       res.render('2FA/index',{
+        Title: 'Google-Authentication',
         user: req.user,
         qrImage: qrImageString,
-        key: encodedKey,
+        secretKey: secretKeyObject.base32,
         messages: messages
       });
 
 });
+
+
+router.post('/', isUserLoggedIn, (req,res,next)=>{
+
+  // getting userEnteredCode and key that is base32Secret hidden value from the form
+  let sixDigitGeneratedToken = req.body.code;
+  let secretKeyBase32 = req.body.key;
+
+  var isVerifiedSixDigitGeneratedTokenAndSecretKeyBase32= speakeasy.totp.verify({
+    secret: secretKeyBase32,
+    encoding: 'base32',
+    token: sixDigitGeneratedToken
+  })
+
+
+  if(isVerifiedSixDigitGeneratedTokenAndSecretKeyBase32)
+  {
+    User.findOneAndUpdate({
+      _id: req.user._id
+    },
+    {
+    secretKey: secretKeyBase32,
+    },
+    function(err, success)
+    {
+      if(err)
+      {
+        console.log(err);
+      }
+      else{
+        console.log(success)
+      }
+    });
+
+    req.session.twoFAAuthenticated = true;
+    res.redirect('/loggedIn');
+  }
+
+  else{
+    req.session.messages = ['Incorrect code or Token, please scan the qr code again and enter the new token or code.'];
+    res.redirect('/2FA/');
+  }
+  
+
+
+})
 
 
 
